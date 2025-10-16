082a1ce29fb6fbd21722127b4d175644f0f5f16f
Shade2231S <matthewmcculloch149@gmail.com>
2025-10-16 00:35:39 -0300

    .

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -6,16 +6,16 @@ public class Camera : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset;
-    public float Distance = 11.0f;
-    public float rotationspeed = 10.0f;
-    private float currentangle = 0f;
-    public bool pressed = false;
+  //  public float Distance = 11.0f;
+   // public float rotationspeed = 10.0f;
+ //   private float currentangle = 0f;
+ //   public bool pressed = false;
 
     [SerializeField] private Camera cam;
     void Start()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        currentangle += horizontalInput * rotationspeed * Time.deltaTime;
+    //    float horizontalInput = Input.GetAxis("Horizontal");
+      //  currentangle += horizontalInput * rotationspeed * Time.deltaTime;
         cam = GetComponent<Camera>();
         offset = transform.position - player.transform.position;
     }
@@ -29,25 +29,25 @@ public class Camera : MonoBehaviour
 
 
     }
-    void Update()
-    {
+  //  void Update()
+   // {
 
-        float x = player.transform.position.x + Distance * Mathf.Sin(currentangle * Mathf.Deg2Rad);
-        float z = player.transform.position.z + Distance * Mathf.Cos(currentangle * Mathf.Deg2Rad);
-        float y = transform.position.y;
+     //   float x = player.transform.position.x + Distance * Mathf.Sin(currentangle * Mathf.Deg2Rad);
+     //   float z = player.transform.position.z + Distance * Mathf.Cos(currentangle * Mathf.Deg2Rad);
+     //   float y = transform.position.y;
         
 
-        transform.position = new Vector3(x, y, z);
+      //  transform.position = new Vector3(x, y, z);
         
-        if (Input.GetMouseButton(1))
-        {
+     //   if (Input.GetMouseButton(1))
+     //   {
             
             
 
-        }
-        if (Input.GetMouseButtonUp(1))
-        {
+     //   }
+     //   if (Input.GetMouseButtonUp(1))
+      //  {
             
-        }
-    }
+      //  }
+ //   }
 }
