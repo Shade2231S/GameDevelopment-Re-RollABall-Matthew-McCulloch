943c226d3d8250073afa46dd12d4ef8aa40000a0
Shade2231S <matthewmcculloch149@gmail.com>
2025-11-05 23:02:11 -0400

    Scene Cleanup
    Made some needed changes to scene1

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -5,49 +5,39 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     [SerializeField] private Camera cam;
-    public GameObject player;
+    public Transform player;
     private Vector3 offset;
-    public int movement;
+    public float rotation = 85f;
 
-    
+
     void Start()
     {
 
         cam = GetComponent<Camera>();
         offset = transform.position - player.transform.position;
     }
-    void movementinput()
-    {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            offset += (transform.forward * movement);
-        }
-        if(Input.GetKey(KeyCode.DownArrow))
-        {
-            offset += (transform.forward * -movement);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            offset += (transform.right * -movement);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            offset += (transform.right * movement);
-        }
-        transform.position = Vector3.Lerp(transform.position, offset, Time.deltaTime * movement);
-    }
+
 
     void LateUpdate()
     {
-        if(player != null)
+        if (player != null)
         {
-           transform.position = player.transform.position + offset;
+            transform.position = player.transform.position + offset;
         }
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    transform.RotateAround(player.position, Vector3.up, rotation * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    transform.RotateAround(player.position, Vector3.up, -rotation * Time.deltaTime);
+        //}
+
+        
+        //transform.LookAt(player);
     }
     void Update()
     {
-        movementinput();
 
     }
-    
 }
